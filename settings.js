42528848c3804e86f47c52c7dc7682707a6ce3ff
Osis.aif <145112589+Https-saiff@users.noreply.github.com>
2025-11-25 00:43:10 +0530

    Update bot settings and owner information
    Signed-off-by: Osis.aif <145112589+Https-saiff@users.noreply.github.com>

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: '𝙰𝙻𝚁𝙴𝙰𝙳𝚈 𝙳𝙴𝙰𝙳',
+  author: '‎osis.aif',
+  botName: "𝙰𝙻𝚁𝙴𝙰𝙳𝚈 𝙳𝙴𝙰𝙳",
+  botOwner: '𝙰𝙻𝚁𝙴𝙰𝙳𝚈 𝙳𝙴𝙰𝙳', // Your name
+  ownerNumber: '918918317218', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.3",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "",
 };
 
 module.exports = settings;
